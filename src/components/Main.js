2c8edfd145d88b89e8999249ae2fd7739f5c78e1
mazrean <shunsuke.w04@gmail.com>
2019-07-22 15:33:24 +0900

    axios追加による不具合?修正

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,61 @@
 import React, { Component }  from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+// axiosのインポート
+import axios from 'axios';
 
 export default class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {text: ''};
+        this.state = {
+            code: '',
+        };
     }
 
+    _onPressButton() {
+        // ボタンが押されたときに、入力した番号をパラメータにし
+        // APIリクエストを送る
+        axios.get('http://zipcloud.ibsnet.co.jp/api/search', {
+            params: {
+                zipcode: this.state.code
+            }
+        })
+        .then(response => {
+            let address = response.data.results
+            var addList = ''
+            for(i=0;address.length>i;i++){
+                addList += address[i].address1 + address[i].address2 + address[i].address3
+            }
+
+            Alert.alert(addList)
+            return
+
+        }).catch(err => {
+            console.log('err:', err);
+            return
+        })
+        
+    }
+
+    
     render() {
         return (
             <View style={styles.container}>
                 <Text style={{padding: 10, fontSize: 20}}>
-                    一言どうぞ
+                    郵便番号を入れてね
                 </Text>
                 <TextInput
-                    style={{height: 40}}
-                    placeholder="ここにメッセージを入れてね!"
-                    onChangeText={(text) => this.setState({text})}
+                    style={{height: 50}}
+                    placeholder="例）0010001"
+                    onChangeText={(code) => this.setState({code})}
                 />
                 <Text>
-                    プレビュー：{this.state.text}
+                    プレビュー：{this.state.code}
                 </Text>
+                <Button
+                    onPress={this._onPressButton.bind(this)}
+                    title="Press Me"
+                    color="red"
+                />
             </View>
         );
     }
